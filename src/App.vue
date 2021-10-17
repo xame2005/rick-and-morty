b7dd2e395ea3059ad3006dbf634188f9ce9adc0b
Samuel Gonzalez <xame2005@gmail.com>
2021-10-17 18:47:28 -0500

    [CREATED] Card component and card styles

--- src/App.vue
+++ src/App.vue
@@ -1,26 +1,46 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div>
+    <h1>Rick and Morty</h1>
+    <ListCharacters />
+  </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
-
+import ListCharacters from "@/components/ListCharacters.vue";
 export default {
-  name: 'App',
-  components: {
-    HelloWorld
-  }
-}
+  name: "App",
+  components: { ListCharacters },
+};
 </script>
 
 <style lang="scss">
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
+:root {
+  --background-body: #24282f;
+  --background-card: #3c3e44;
+  --text-white: #ffffff;
+  --text-gray: #c0c0c0;
+  --text-orange: #ff9800;
+}
+* {
+  box-sizing: border-box;
+  margin: 0;
+  padding: 0;
+}
+body {
+  background-color: var(--background-body);
+  color: var(--text-white);
+  font-family: "Segoe UI", Tahoma, Geneva, Verdana, sans-serif;
+}
+h1 {
+  margin-bottom: 3rem;
   text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+}
+.container {
+  width: 980px;
+  max-width: 90%;
+  margin: 5rem auto;
+}
+img {
+  width: 100%;
 }
 </style>
--- src/store/index.js
+++ src/store/index.js
@@ -1,12 +1,34 @@
-import { createStore } from 'vuex'
+import { createStore } from "vuex";
 
 export default createStore({
   state: {
+    characters: [],
+    charactersFilter: [],
   },
   mutations: {
+    setCharacters(state, payload) {
+      state.characters = payload;
+    },
+
+    setCharactersFilter(state, payload) {
+      state.charactersFilter = payload;
+    },
   },
   actions: {
+    async getCharacters({ commit }) {
+      try {
+        const response = await fetch(
+          "https://rickandmortyapi.com/api/character"
+        );
+
+        const data = await response.json();
+
+        commit("setCharacters", data.results);
+        commit("setCharactersFilter", data.results);
+      } catch (error) {
+        console.error(error);
+      }
+    },
   },
-  modules: {
-  }
-})
+  modules: {},
+});
